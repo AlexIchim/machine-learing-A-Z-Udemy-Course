9c8b4f7c7340ec5bf4995da538093d3debe25609
Alexandru Ichim <alexichim94@yahoo.ro>
2017-01-15 20:00:59 +0200

    Added Decision Tree Regression Python.

---
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,56 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+
+# Matrix of features 
+# Take all the lines and all the columns except the last one -1
+X = dataset.iloc[:, 1:2].values
+
+# Dependend variable vector
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+# Feature Scaling only on X not on y (just for regression we feature scale y)
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Filling Simple Linear Regression to the Training Set
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor( random_state = 0 )
+regressor.fit(X, y)
+
+# Predicting the Test set results
+y_pred = regressor.predict(6.5)
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+# plot the predictions
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
